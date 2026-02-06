05b02a440d3b4ad8804deb82313617dedbdb9cea
shahithkumar <shahithu2004@gmail.com>
2026-02-06 07:34:44 +0530

    fix: remove mysqlclient and update python version for Vercel

--- backend/delivery_api/build_files.sh
+++ backend/delivery_api/build_files.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate --noinput
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput
+python3.12 manage.py migrate --noinput
